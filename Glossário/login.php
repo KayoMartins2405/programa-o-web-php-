4e4e732b88cd7b43897cf699339c35614bc63dde
KayoMartins2405 <martinslima24242gmail.com>
2022-09-16 20:42:25 -0300

    projeto

---
+++ Glossário/login.php
@@ -0,0 +1,18 @@
+<section>
+    <form>
+        <div>
+            <input type="text " placeholder= "Usuário">
+        </div>
+    
+        <div>
+            <input type="text " placeholder= "Login">
+        </div>
+        
+        <div>
+            <input type="text " placeholder= "Senha">
+        </div>
+        
+       
+    
+    </form>
+</section>
---
+++ Glossário/palavra-letra.php
@@ -0,0 +1,12 @@
+<section>
+
+    <figure>
+
+
+    
+    </figure>
+
+
+
+
+</section>
